4fe68293c3f55f4ca7ac27ec0897341519de002d
Katlen Duarte <katlenduarte@gmail.com>
2025-10-18 22:17:40 -0300

    correção

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // outras opções aqui
+  images: {
+    domains: [
+      "images.pexels.com",
+    ],
+  },
 };
 
 export default nextConfig;
